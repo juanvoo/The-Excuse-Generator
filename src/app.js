0c5f5e38f26cfb96a5cfacd4b2f3ea43c44e9e93
juanvoo <juanmoreiracosta2016@gmail.com>
2022-09-26 18:35:28 +0000

    Realizacion de generador de excusas

--- src/app.js
+++ src/app.js
@@ -3,31 +3,23 @@ import "./style.css";
 
 window.onload = () => {
   let randomNumber = Math.random() * 10;
-  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
 };
 let generateExcuse = () => {
-  let pronoun = ["A", "The"];
-  let subject = ["comedian", "driver", "jogger"];
-  let action = ["stole", "kicked", "burned"];
-  let possetion = ["my backpack", "my motorbike", "my t-shirt"];
-  let where = ["in my house", "in the school", "on the road"];
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-  let proIndx = Math.floor(Math.random() * pronoun.length);
-  let subjIndx = Math.floor(Math.random() * subject.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let possetionIndex = Math.floor(Math.random() * possetion.length);
-  let whereIndex = Math.floor(Math.random() * where.length);
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
 
-  return (
-    pronoun[proIndx] +
-    " " +
-    subject[subjIndx] +
-    " " +
-    action[actionIndex] +
-    " " +
-    possetion[possetionIndex] +
-    " " +
-    where[whereIndex]
-  );
+  return who[whoIndx] + " " + what[whatIndx] + " " + when[whenIndex];
 };
